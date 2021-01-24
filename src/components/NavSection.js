572159c7ea4661dae439b5184ad5db7a9f60ded6
Your Name <epyon85@gmail.com>
2021-01-24 15:07:35 -0500

    uploading

--- src/components/NavSection.js
+++ src/components/NavSection.js
@@ -10,15 +10,15 @@ class NavSection extends React.Component{
                 <div className="row">
                     <div className="col-md-12">
                         <div className="nav-container">
-                            <Navbar variant="dark" expand="lg" className="navwrapper__custom-nav justify-content evenly" >
+                         <Navbar variant="dark" expand="lg" className="navwrapper__custom-nav justify-content-evenly" >
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                                <Navbar.Collapse id="basic-navbar-nav" className="ml-auto">
-                                    <Nav className="mx-auto w-75">
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className="ml-auto">
                                     <Nav.Link href="/" active>Home</Nav.Link>
                                     <Nav.Link href="/pharmacy" active>Pharmacy</Nav.Link>
                                     <Nav.Link href="/products" active>Products</Nav.Link>
                                     <Nav.Link href="/account" active>Account</Nav.Link>
-                                    <Nav.Link href="/about" active>About Us</Nav.Link>
+                                    <Nav.Link href="/about"active>About Us</Nav.Link>
                                     </Nav>
                                     <Form inline>
                                     <FormControl type="text" placeholder="Search..." className="mr-sm-2" />
