0b3a47eeeb7f5cfbcc953c2b46a5865ea5ab5a6b
Your Name <epyon85@gmail.com>
2021-01-24 14:43:15 -0500

    gh pages

--- src/components/NavSection.js
+++ src/components/NavSection.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Navbar,Nav, Form, FormControl, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
 class NavSection extends React.Component{
@@ -11,7 +10,7 @@ class NavSection extends React.Component{
                 <div className="row">
                     <div className="col-md-12">
                         <div className="nav-container">
-                            <Navbar variant="dark" expand="lg" className="navwrapper__custom-nav justify-content-evenly" >
+                            <Navbar variant="dark" expand="lg" className="navwrapper__custom-nav justify-content evenly" >
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav" className="ml-auto">
                                     <Nav className="mx-auto w-75">
@@ -19,7 +18,7 @@ class NavSection extends React.Component{
                                     <Nav.Link href="/pharmacy" active>Pharmacy</Nav.Link>
                                     <Nav.Link href="/products" active>Products</Nav.Link>
                                     <Nav.Link href="/account" active>Account</Nav.Link>
-                                    <Nav.Link href="/about"active>About Us</Nav.Link>
+                                    <Nav.Link href="/about" active>About Us</Nav.Link>
                                     </Nav>
                                     <Form inline>
                                     <FormControl type="text" placeholder="Search..." className="mr-sm-2" />
