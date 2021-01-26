b334f7affb598ebe255031b92e610f0d063dcc75
Your Name <epyon85@gmail.com>
2021-01-25 19:12:43 -0500

    primary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component }from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import NavSection from './components/NavSection';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import { Pharmacy } from './components/Pharmacy';
 import Products from './components/Products/Index';
@@ -16,7 +16,7 @@ class App extends Component {
   render() {
     return(
       <React.Fragment>
-          <NavSection />
+          <Navbar />
           <Router>
               <Switch>
                 <Route exact path="/" component={Home} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,23 +6,23 @@ class NavSection extends React.Component{
 
     render(){
         return(
-            <div className="navwrapper">
+            <div>
                 <div className="row">
                     <div className="col-md-12">
                         <div className="nav-container">
-                         <Navbar variant="dark" expand="lg" className="navwrapper__custom-nav justify-content-evenly" >
+                            <Navbar bg="primary" variant="dark" expand="lg" >
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
-                                    <Nav className="ml-auto">
+                                    <Nav className="mx-auto w-75">
                                     <Nav.Link href="/" active>Home</Nav.Link>
                                     <Nav.Link href="/pharmacy" active>Pharmacy</Nav.Link>
                                     <Nav.Link href="/products" active>Products</Nav.Link>
                                     <Nav.Link href="/account" active>Account</Nav.Link>
-                                    <Nav.Link href="/about" active>About Us</Nav.Link>
+                                    <Nav.Link href="/about"active>About Us</Nav.Link>
                                     </Nav>
                                     <Form inline>
                                     <FormControl type="text" placeholder="Search..." className="mr-sm-2" />
-                                    <Button variant="outline-info">Search</Button>
+                                    <Button variant="outline-danger">Search</Button>
                                     </Form>
                                 </Navbar.Collapse>
                             </Navbar>
